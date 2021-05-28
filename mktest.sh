b753bc1446a087d8d0e5ed0c5b79d353265b5d3e
Thibaud PIRES <thibaud.pires@orange.fr>
2021-05-28 14:17:38 +0200

    Added Test Framework

---
+++ mktest.sh
@@ -0,0 +1,1 @@
+( cd Tests ; mktest.sh )
--- src/Model/Cleaner.cpp
+++ src/Model/Cleaner.cpp
@@ -7,9 +7,12 @@ Cleaner::Cleaner(Coords c, time_t begin, time_t end, string id)
     this->endTime = end;
     this->id = id;
 }
+
 Cleaner::Cleaner() : coords(), beginTime(), endTime()
 {
+    
 }
+
 Cleaner::~Cleaner()
 {
 }
