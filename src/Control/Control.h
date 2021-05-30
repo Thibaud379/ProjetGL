1e9bab67911aa10dfffc679572f96d44f9abe4a2
Benoit69110 <80839027+Benoit69110@users.noreply.github.com>
2021-05-30 14:55:12 +0200

    Update Control.h
    Constructeur par défaut

--- src/Control/Control.h
+++ src/Control/Control.h
@@ -16,6 +16,7 @@ public:
     list<pair<float, Sensor>> *findSimilarSensor(Sensor target, float deltaMax, time_t date);
     int getAirQuality(Coords coords, int radius, time_t start, time_t end);
     vector<int> getImpact(Cleaner target, int radius);
+    Control();
     Control(unordered_map<string, string> files);
     ~Control();
 };
